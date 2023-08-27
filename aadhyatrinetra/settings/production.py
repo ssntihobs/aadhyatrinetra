f0766f88b945e8d1be889335b98ac6d72c9c28e7
Vishal Arpit Kumar <vis.cas.91@gmail.com>
2023-08-28 00:21:42 +0530

    modified hosts 2

--- aadhyatrinetra/settings/production.py
+++ aadhyatrinetra/settings/production.py
@@ -1,7 +1,7 @@
 from .base import *
 
 SECRET_KEY = 'EU9Cpozke$dLCbk2$~byv@fa2amF9$p4~RckzSPi#HZTydQ$NQgZ$ECc29$kugJXD#59YPbmYsZrW59gW@`@k3vVKZVZrLZ43ctK'
-ALLOWED_HOSTS = ["impelverse.com", "www.impelverse.com"]
+ALLOWED_HOSTS = ["impelverse.com"]
 
 DEBUG = False
 
