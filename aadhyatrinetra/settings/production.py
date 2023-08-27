bec13384dec3bf9c4b6d660a1b86bbe91b0477db
Vishal Arpit Kumar <vis.cas.91@gmail.com>
2023-08-28 00:33:42 +0530

    added csrf_trusted_origins setting

--- aadhyatrinetra/settings/production.py
+++ aadhyatrinetra/settings/production.py
@@ -2,6 +2,7 @@ from .base import *
 
 SECRET_KEY = 'EU9Cpozke$dLCbk2$~byv@fa2amF9$p4~RckzSPi#HZTydQ$NQgZ$ECc29$kugJXD#59YPbmYsZrW59gW@`@k3vVKZVZrLZ43ctK'
 ALLOWED_HOSTS = ["https://impelverse.com"]
+CSRF_TRUSTED_ORIGINS = ["https://impelverse.com"]
 
 DEBUG = False
 
