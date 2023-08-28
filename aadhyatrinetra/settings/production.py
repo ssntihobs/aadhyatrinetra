5a0ea0d91c6f58d1a7b2a75993db64c425aa9904
Vishal Arpit Kumar <vis.cas.91@gmail.com>
2023-08-28 18:24:24 +0530

    modified schema

--- aadhyatrinetra/settings/production.py
+++ aadhyatrinetra/settings/production.py
@@ -2,9 +2,9 @@ from .base import *
 
 SECRET_KEY = 'EU9Cpozke$dLCbk2$~byv@fa2amF9$p4~RckzSPi#HZTydQ$NQgZ$ECc29$kugJXD#59YPbmYsZrW59gW@`@k3vVKZVZrLZ43ctK'
 ALLOWED_HOSTS = ['impelverse.com', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['https://impelverse.com', '127.0.0.1']
-CSRF_ALLOWED_ORIGINS = ["https://impelverse.com", '127.0.0.1']
-CORS_ORIGINS_WHITELIST = ["https://impelverse.com", '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['impelverse.com', '127.0.0.1']
+CSRF_ALLOWED_ORIGINS = ["impelverse.com", '127.0.0.1']
+CORS_ORIGINS_WHITELIST = ["impelverse.com", '127.0.0.1']
 
 DEBUG = True
 
