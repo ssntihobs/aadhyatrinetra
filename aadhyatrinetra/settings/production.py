81d9a8806676c332fcdcae948247fe402ff9807f
Vishal Arpit Kumar <vis.cas.91@gmail.com>
2023-09-06 13:40:13 +0530

    added new config in settings

--- aadhyatrinetra/settings/production.py
+++ aadhyatrinetra/settings/production.py
@@ -1,10 +1,10 @@
 from .base import *
 
 SECRET_KEY = 'EU9Cpozke$dLCbk2$~byv@fa2amF9$p4~RckzSPi#HZTydQ$NQgZ$ECc29$kugJXD#59YPbmYsZrW59gW@`@k3vVKZVZrLZ43ctK'
-ALLOWED_HOSTS = ['impelverse.com', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['impelverse.com', '127.0.0.1']
-CSRF_ALLOWED_ORIGINS = ["impelverse.com", '127.0.0.1']
-CORS_ORIGINS_WHITELIST = ["impelverse.com", '127.0.0.1']
+ALLOWED_HOSTS = ['impelverse.com', '143.110.244.126']
+CSRF_TRUSTED_ORIGINS = ['impelverse.com', '143.110.244.126']
+CSRF_ALLOWED_ORIGINS = ["impelverse.com", '143.110.244.126']
+CORS_ORIGINS_WHITELIST = ["impelverse.com", '143.110.244.126']
 
 DEBUG = True
 
